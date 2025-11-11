c4faf4ad932591d2a1755aa6b50168ca0f019216
Nour Hossam Ahmed <nour.farahat@student.giu-uni.de>
2025-11-11 09:44:03 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,8 +4,23 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
 
+class Course: public Person{
+private: 
+string courseCode;
+string courseName;
+int maxStudents;
+Student* students;
+int currentStudents;
+public: 
+Course(string courseCode, string courseName, int maxStudents, Student* students, int currentStudents);
+Course();
+~Course();
+
+addStudent(const Student& s);
+void displayCourseInfo();
+
+};
 
 
 

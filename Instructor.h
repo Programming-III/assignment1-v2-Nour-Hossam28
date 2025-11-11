8397a943f4e5356e54b103a361a70ebecb9ac7ca
Nour Hossam Ahmed <nour.farahat@student.giu-uni.de>
2025-11-11 09:44:13 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,15 +1,19 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
-
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+class Instructor: public Person{
+private:
+string department;
+int experienceYears;
+public:
+Instructor(stirng department, int experienceYears);
+Instructor();
+~Instructor();
+void display();
 
 
-
-
-
-
+};
 
 
 
